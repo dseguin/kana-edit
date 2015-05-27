cf5541aca101f7fe5ad2e882f6aab3adcf7ea3ac
dseguin <davidseguin@live.ca>
2015-05-26 23:19:10 -0400

    Tweeked autodefine script

--- autodefine.sh
+++ autodefine.sh
@@ -1,15 +1,24 @@
 #! /bin/sh
 
-echo "Running autodefine.sh" | tee autodefine.log
+echo "\nRunning autodefine.sh" | tee autodefine.log
+
+# find absolute path
+DIR=$( cd "$( dirname "$0" )" && pwd )
+
+echo "kana-edit directory = $DIR\n"
 
 INSTALLED=0
 
 # installed font check
 if [ -f /usr/share/fonts/truetype/msttcorefonts/arial.ttf ] ; then
 	
-	echo "WARNING: Detected 'arial.ttf' in standard font folder. Changing FONT_ARIAL to /usr/share/fonts/truetype/msttcorefonts/arial.ttf" | tee -a autodefine.log
+	if ! grep -rl msttcorefonts/arial.ttf include/ >/dev/null ; then
+		
+		echo "WARNING: Detected 'arial.ttf' in standard font folder. Changing FONT_ARIAL to /usr/share/fonts/truetype/msttcorefonts/arial.ttf" | tee -a autodefine.log
 	
-	grep -rl resources/arial.ttf include/ | xargs sed -i 's~resources/arial.ttf~/usr/share/fonts/truetype/msttcorefonts/arial.ttf~' | tee -a autodefine.log
+		grep -rl resources/arial.ttf include/ | xargs sed -i 's~resources/arial.ttf~/usr/share/fonts/truetype/msttcorefonts/arial.ttf~' | tee -a autodefine.log
+
+	fi
 
 	INSTALLED=1
 
@@ -20,12 +29,11 @@ else
 fi
 
 # find absolute path
-# DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-DIR=$( cd "$( dirname "$0" )" && pwd )
+#DIR=$( cd "$( dirname "$0" )" && pwd )
 
-echo "$DIR"
+#echo "\nkana-edit directory = $DIR\n"
 
-if ! grep -rl kana-edit/resources/mangal include/  ; then
+if ! grep -rl /resources/mangal include/ >/dev/null ; then
 
 	echo "WARNING: Replacing FONT_MANGAL with $DIR/resources/mangal.ttf. If you ran this script more than once, this path may be wrong. This can be fixed by changing it in include/defines.h" | tee -a autodefine.log
 	grep -rl resources/mangal.ttf include/ | xargs sed -i 's~resources/mangal.ttf~'$DIR'/resources/mangal.ttf~' | tee -a autodefine.log
@@ -34,7 +42,12 @@ fi
 
 if [ ! $INSTALLED = 1 ] ; then
 
-	echo "WARNING: Replacing FONT_ARIAL with $DIR/resources/arial.ttf. If you ran this script more than once, this path may be wrong. This can be fixed by changing it in include/defines.h" | tee -a autodefine.log
-	grep -rl resources/arial.ttf include/ | xargs sed -i 's~resources/arial.ttf~'$DIR'/resources/arial.ttf~' | tee -a autodefine.log
+	if ! grep -rl /resources/arial include/ >/dev/null ; then
+	
+		echo "WARNING: Replacing FONT_ARIAL with $DIR/resources/arial.ttf. If you ran this script more than once, this path may be wrong. This can be fixed by changing it in include/defines.h" | tee -a autodefine.log
+	
+		grep -rl resources/arial.ttf include/ | xargs sed -i 's~resources/arial.ttf~'$DIR'/resources/arial.ttf~' | tee -a autodefine.log
+	
+	fi
 
 fi
